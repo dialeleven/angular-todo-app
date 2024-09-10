1fd641303f30702981cda119269746348e7d0514
Francis Tsao <dialeleven@gmail.com>
2024-09-10 02:46:02 -0400

    dynamic property for [checked] in todo-list-item.component.html

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -13,10 +13,11 @@ import { TodoListItemComponent } from '../todo-list-item/todo-list-item.componen
 })
 export class TodoListComponent {
   defaultTasksList: any[] = [
-    { id: 1, text: "Edit item   - watch out for long text lines that wrap", duedate: "2099-01-01 12:00", completed: true, position: 1 },
-    { id: 2, text: "Add todo item", duedate: "", completed: true, position: 2 },
-    { id: 3, text: "Delete todo item", duedate: "", completed: true, position: 3 },
-    { id: 4, text: "localStorage save/read of todo items", duedate: "", completed: true, position: 4 },
+    { id: 0, text: "Update checkbox state for completed todo items", duedate: "2099-01-01 12:00", completed: true, position: 0 },
+    { id: 1, text: "Edit item - watch out for long text lines that wrap", duedate: "2099-01-01 12:00", completed: false, position: 1 },
+    { id: 2, text: "Add todo item", duedate: "", completed: false, position: 2 },
+    { id: 3, text: "Delete todo item", duedate: "", completed: false, position: 3 },
+    { id: 4, text: "localStorage save/read of todo items", duedate: "", completed: false, position: 4 },
     { id: 5, text: "Drag and drop reordering of todo items", duedate: "2024-08-01 12:00", completed: false, position: 5 }
   ];
 }
