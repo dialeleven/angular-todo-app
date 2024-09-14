d472cbc11a61cb797aac0bb8c8009c07985c653c
Francis Tsao <dialeleven@gmail.com>
2024-09-13 23:38:07 -0400

    delete todo item done

--- src/app/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-item/todo-list-item.component.ts
@@ -10,14 +10,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class TodoListItemComponent {
   @Input() task: any;
   @Output() taskUpdated = new EventEmitter<any>();
+  @Output() deleteTask = new EventEmitter<number>();
 
-  // toggled completed value for the task
-  toggleCompleted() {
-     this.task.completed = !this.task.completed; // toggle completed state
-     this.taskUpdated.emit(this.task); // emit the updated task to the parent component
-  }
-  
-  handleEdit() {
-    // logic to handle editing a task
-  }
+// toggled completed value for the task
+toggleCompleted() {
+   this.task.completed = !this.task.completed; // toggle completed state
+   this.taskUpdated.emit(this.task); // emit the updated task to the parent component
+}
+
+handleDelete() {
+  this.deleteTask.emit(this.task.id); // Emit the task's ID to the parent component
+}
+
+handleEdit() {
+  // logic to handle editing a task
+}
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -16,7 +16,7 @@ export class TodoListComponent {
     { id: 0, text: "Update checkbox state for completed todo items", duedate: "2099-01-01 12:00", completed: true, position: 0 },
     { id: 1, text: "Edit item - watch out for long text lines that wrap", duedate: "2099-01-01 12:00", completed: false, position: 1 },
     { id: 2, text: "Add todo item", duedate: "", completed: false, position: 2 },
-    { id: 3, text: "Delete todo item", duedate: "", completed: false, position: 3 },
+    { id: 3, text: "Delete todo item", duedate: "", completed: true, position: 3 },
     { id: 4, text: "localStorage save/read of todo items", duedate: "", completed: false, position: 4 },
     { id: 5, text: "Drag and drop reordering of todo items", duedate: "2024-08-01 12:00", completed: false, position: 5 }
   ];
@@ -55,5 +55,11 @@ export class TodoListComponent {
     } else if (this.filterType === 'tasks-unchecked') {
       this.filteredTasksList = this.defaultTasksList.filter(task => !task.completed);
     }
+  }
+
+  // Method to handle the delete event from the child component
+  onDeleteTask(taskId: number) {
+    this.defaultTasksList = this.defaultTasksList.filter(task => task.id !== taskId);
+    this.filteredTasksList = [...this.defaultTasksList]; // Update filtered list after deletion
   }
 }
