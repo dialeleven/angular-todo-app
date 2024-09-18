2bbcdccc2170b864dbf649978795199573fae115
Francis Tsao <dialeleven@gmail.com>
2024-09-18 07:10:31 -0400

    Modal window for add action logic ("Add" labels, Close buttons and Esc key event)

--- src/app/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-item/todo-list-item.component.ts
@@ -12,17 +12,20 @@ export class TodoListItemComponent {
   @Output() taskUpdated = new EventEmitter<any>();
   @Output() deleteTask = new EventEmitter<number>();
 
-// toggled completed value for the task
-toggleCompleted() {
-   this.task.completed = !this.task.completed; // toggle completed state
-   this.taskUpdated.emit(this.task); // emit the updated task to the parent component
-}
+  // toggled completed value for the task
+  toggleCompleted() {
+    this.task.completed = !this.task.completed; // toggle completed state
+    this.taskUpdated.emit(this.task); // emit the updated task to the parent component
+  }
 
-handleDelete() {
-  this.deleteTask.emit(this.task.id); // Emit the task's ID to the parent component
-}
+  handleDelete() {
+    this.deleteTask.emit(this.task.id); // Emit the task's ID to the parent component
+  }
 
-handleEdit() {
-  // logic to handle editing a task
-}
+  handleEdit() {
+    // logic to handle editing a task
+    console.log('handleEdit called');
+
+    // show the modal
+  }
 }
--- src/app/todo-list-modal/todo-list-modal.component.ts
+++ src/app/todo-list-modal/todo-list-modal.component.ts
@@ -1,17 +1,40 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list-modal',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Include FormsModule here
   templateUrl: './todo-list-modal.component.html',
   styleUrl: './todo-list-modal.component.css'
 })
 export class TodoListModalComponent {
   @Input() showModal: boolean = false; // used to show/hide the modal
+  @Input() addEditMode: string = 'Add'; // label for modal heading
   @Output() closeModal = new EventEmitter<void>(); // used to close the modal
+  
+  // Used to focus the input field when the modal is displayed
+  @ViewChild('todoInput') todoInput!: ElementRef; // Reference to the input field
 
-  close() {
+  // Define todoTextInput to store the value from the input field
+  todoTextInput: string = ''; 
+  
+  // Focus the input when the modal is displayed using ngAfterViewInit() lifecycle 
+  // hook in Angular. It is called after Angular has fully initialized a component's view.
+  // In this case, it is used to focus an input field when a modal is displayed.
+  ngAfterViewInit() {
+    // Focus the input when the modal is displayed
+    if (this.showModal) {
+      this.todoInput.nativeElement.focus();
+    }
+  }
+
+  handleClose() {
     this.closeModal.emit(); // emit close modal event
+  }
+
+  handleSubmit() {
+    // logic to handle submitting the form
+    console.log('handleSubmit called');
   }
 }
