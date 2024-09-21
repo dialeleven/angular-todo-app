f7a1e68097b0c2a796bc19b3a33dc05c2c0a1cb8
Francis Tsao <dialeleven@gmail.com>
2024-09-20 21:42:16 -0400

    defaultTasks update for localStorage task

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -153,7 +153,7 @@ export class TodoListComponent {
       { id: 1, text: "Edit item - watch out for long text lines that wrap", duedate: "2099-01-01 12:00", completed: true, position: 1 },
       { id: 2, text: "Add todo item", duedate: "", completed: true, position: 2 },
       { id: 3, text: "Delete todo item", duedate: "", completed: true, position: 3 },
-      { id: 4, text: "localStorage save/read of todo items", duedate: "", completed: false, position: 4 },
+      { id: 4, text: "localStorage save/read of todo items", duedate: "", completed: true, position: 4 },
       { id: 5, text: "Drag and drop reordering of todo items", duedate: "2024-08-01 12:00", completed: false, position: 5 }
     ];
 
