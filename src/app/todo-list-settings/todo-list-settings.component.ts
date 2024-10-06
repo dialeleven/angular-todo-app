5a0fb483cbb31621308d1a541f0b497b1442d79e
Francis Tsao <dialeleven@gmail.com>
2024-10-06 18:55:35 -0400

    Added "back to todo app" routerlink anchor tag. Added `RouterModule` import to todo-list-settings.component.ts

--- src/app/todo-list-settings/todo-list-settings.component.ts
+++ src/app/todo-list-settings/todo-list-settings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list-settings',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './todo-list-settings.component.html',
   styleUrl: './todo-list-settings.component.css'
 })
