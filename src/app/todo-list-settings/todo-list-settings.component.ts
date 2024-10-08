94420356966047bb5c8306cf23136d794cc2b535
Francis Tsao <dialeleven@gmail.com>
2024-10-07 21:25:34 -0400

    show "add todo" button on bottom of list checkbox state saved to and loaded from localStorage

--- src/app/todo-list-settings/todo-list-settings.component.ts
+++ src/app/todo-list-settings/todo-list-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { provideRouter, RouterModule } from '@angular/router';
 
 @Component({
@@ -9,5 +9,24 @@ import { provideRouter, RouterModule } from '@angular/router';
   styleUrl: './todo-list-settings.component.css'
 })
 export class TodoListSettingsComponent {
+  @Input() showAddTodoButtonBottom: boolean = false; 
 
+  // on component initialization, load settings from local storage
+  ngOnInit() {
+    const showTodoControlsOnBottom = localStorage.getItem('showTodoControlsOnBottom');
+
+    // check/uncheck show add todo button on bottom from localstorage if it exists
+    if (showTodoControlsOnBottom) {
+      this.showAddTodoButtonBottom = showTodoControlsOnBottom === 'true';
+    }
+  }
+
+  toggleShowTodoControlsOnBottom() {
+    this.showAddTodoButtonBottom = !this.showAddTodoButtonBottom;
+
+    console.log('toggleShowTodoControlsOnBottom, ', this.showAddTodoButtonBottom);
+
+    // save show add todo button on bottom to local storage
+    localStorage.setItem('showTodoControlsOnBottom', this.showAddTodoButtonBottom ? 'true' : 'false');
+  }
 }
