8f53dbfe86039b1c53c4f678fcf244196ff218dd
Francis Tsao <dialeleven@gmail.com>
2024-10-28 19:43:49 -0400

    removed default /todo path due to Netlify page reload/refresh issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListSettingsComponent } from './todo-list-settings/todo-list-settings.component';
 
 export const routes: Routes = [
-   { path: '', redirectTo: 'todo', pathMatch: 'full' },
-   { path: 'todo', component: TodoListComponent },
+   { path: '', component: TodoListComponent },
    { path: 'settings', component: TodoListSettingsComponent },
    // { path: 'other', component: SomeOtherComponent },
-   { path: '**', redirectTo: 'todo' } // Wildcard route for a 404 page
+   { path: '**', redirectTo: '' } // Wildcard route for a 404 page
  ];
